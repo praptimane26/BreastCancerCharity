8050d8572938d944c6f751d124609b2ebfa7c2f4
praptimane26 <praptimane26@github.com>
2021-03-29 00:12:59 +1300

    Front End
    Added a button to add new tasks
    also the backend is working fine the new task is getting added and can be seen in the console and on postman

--- frontend/src/app/pages/task-view/task-view.component.ts
+++ frontend/src/app/pages/task-view/task-view.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 import { TaskService } from 'src/app/task.service';
+
 
 @Component({
   selector: 'app-task-view',
@@ -8,14 +10,25 @@ import { TaskService } from 'src/app/task.service';
 })
 export class TaskViewComponent implements OnInit {
 
-  constructor(private taskService: TaskService) { }
+  lists: any[];
+  tasks: any[];
+
+
+  constructor(private taskService: TaskService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-  }
+    this.route.params.subscribe (
+      (params: Params) => {
+        console.log(params); 
+      this.taskService.getTasks(params.listId).subscribe((tasks:any[]) => {
+        this.tasks = tasks;
+      })
+      }
 
-  createNewList() {
-    this.taskService.createList('Testing').subscribe((response: any) => {
-      console.log(response);
+    )
+
+    this.taskService.getLists().subscribe((lists: any[]) => {
+      this.lists = lists;
     })
   }
 }
