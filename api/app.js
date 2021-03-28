fb6cdb141da40b46e120e195355dab93d842eea5
praptimane26 <praptimane26@github.com>
2021-03-28 18:42:08 +1300

    New list getting added

--- api/app.js
+++ api/app.js
@@ -11,6 +11,16 @@ const { List, Task } = require("./db/models");
 //Load Middleware
 app.use(bodyParser.json());
 
+//CORS Headers Middleware
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 //Route Handlers
 
 //List Routes
