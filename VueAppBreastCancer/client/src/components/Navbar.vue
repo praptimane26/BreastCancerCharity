0775c03e5267d6655ab0bc965b0f9d3e43db5ca2
praptimane26 <praptimane26@github.com>
2021-05-25 14:09:49 +1200

    commit

---
+++ VueAppBreastCancer/client/src/components/Navbar.vue
@@ -0,0 +1,5 @@
+<template>
+  <p>NAVVVVV</p>
+</template>
+
+
--- VueAppBreastCancer/client/src/main.js
+++ VueAppBreastCancer/client/src/main.js
@@ -5,8 +5,8 @@ createApp(App).mount('#app')*/
 
 import Vue from "vue";
 import App from "./App.vue";
-import { router } from "./router";
-import store from "./store";
+import { VueRouter } from "vue-router";
+//import store from "./store";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import VeeValidate from "vee-validate";
@@ -20,6 +20,8 @@ import {
   faSignOutAlt,
 } from "@fortawesome/free-solid-svg-icons";
 
+import { nav } from "./components/Navbar.vue"
+
 library.add(faHome, faUser, faUserPlus, faSignInAlt, faSignOutAlt);
 
 Vue.config.productionTip = false;
@@ -27,8 +29,18 @@ Vue.config.productionTip = false;
 Vue.use(VeeValidate);
 Vue.component("font-awesome-icon", FontAwesomeIcon);
 
+Vue.use(VueRouter);
+
+const routes = [
+    { path: '/login', component: nav }
+]
+
+const router = new VueRouter ({
+    routes,
+    mode: 'history'
+})
+
 new Vue({
-  router,
-  store,
-  render: (h) => h(App),
+    render: (h) => h(App),
+    router,
 }).$mount("#app");
