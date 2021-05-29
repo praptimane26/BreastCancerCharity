3e6cfbc4bb446b636f74fe08e9714b3961a515f9
praptimane26 <praptimane26@github.com>
2021-05-29 20:38:15 +1200

    backend is working fine now

--- VueBreastCancerBackend/server.js
+++ VueBreastCancerBackend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const dbConfig = require("./app/config/db.config");
 
 const app = express();
 
@@ -37,6 +38,10 @@ db.mongoose
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to bezkoder application." });
 });
+
+// routes
+require("./app/routes/auth.routes")(app);
+require("./app/routes/user.routes")(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
