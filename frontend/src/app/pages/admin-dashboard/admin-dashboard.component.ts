2f1adf913c48131c00390f06103b068302aa7607
praptimane26 <praptimane26@github.com>
2021-06-24 11:13:53 +1200

    bookings not reflecting in front end

--- frontend/src/app/pages/admin-dashboard/admin-dashboard.component.ts
+++ frontend/src/app/pages/admin-dashboard/admin-dashboard.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { AdminService } from 'src/app/admin.service';
 import { Booking } from 'src/app/models/booking.model';
+import { List } from 'src/app/models/list.model';
+import { TaskService } from 'src/app/task.service';
+
 
 @Component({
   selector: 'app-admin-dashboard',
@@ -10,8 +13,9 @@ import { Booking } from 'src/app/models/booking.model';
 export class AdminDashboard implements OnInit {
 
 bookings: Booking []
+lists: List[];
 
-  constructor(private adminService: AdminService) { }
+  constructor(private adminService: AdminService, private taskService: TaskService) { }
 
   ngOnInit(): void {
       console.log("in ts")
@@ -21,12 +25,9 @@ bookings: Booking []
     //   })
    
     this.adminService.getBookings().subscribe((bookings: Booking[] ) => {
-        console.log("in seives")
-        this.bookings = bookings
-            console.log(bookings)
-    }
-    )
-
+        this.bookings = bookings;
+            console.log(bookings)});
+            
     console.log("in ts 2")
 
 
